dcedfad22d8d38d00e3caa4fd7bf3bdcdb8b72a3
zhangyu <2016107303@qq.com>
2021-01-10 13:31:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div >
+    <router-view></router-view>
+    <footer-guide></footer-guide>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+ components:{
+   FooterGuide
+ }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      footer
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='stylus'>
+body 
+  color red
+</style>
